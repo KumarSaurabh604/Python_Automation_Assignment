2aa2878c73214fc39ee6555aa270e8a556180f64
Saurabh Kumar <saurabh.kumar04@nagarro.com>
2024-09-09 18:06:41 +0530

    udpate file

--- uibddpytest/conftest.py
+++ uibddpytest/conftest.py
@@ -29,7 +29,7 @@ def pytest_runtest_makereport(item, call):
 
 @pytest.hookimpl(tryfirst=True)
 def pytest_configure(config):
-    report_dir = Path('uibddpytest/reports/htmlReport')
+    report_dir = Path('reports/htmlReport')
     report_dir.mkdir(parents=True, exist_ok=True)
     config.option.htmlpath = report_dir / "report.html"
     config.option.self_contained_html = True
